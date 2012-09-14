fe97e0f83001d4e34e38ac0d2029d023b6672b96
Seamus Abshere <seamus@abshere.net>
2012-09-13 20:12:13 -0500

    test sqlite and mysql

--- tests.py
+++ tests.py
@@ -1,23 +1,20 @@
 import unittest
 import psycopg2
+import MySQLdb
+import sqlite3
 
 from upsert import *
 
-class TestUpsertPostgresql(unittest.TestCase):
-    def setUp(self):
-        self.connection = psycopg2.connect('dbname=test_py_upsert')
-        cur = self.connection.cursor()
-        cur.execute("""
-        DROP TABLE IF EXISTS pets;
+def create_table(connection):
+    cur = connection.cursor()
+    cur.execute("""
+    DROP TABLE IF EXISTS pets;
 """)
-        cur.execute("""
-        CREATE TABLE pets ( "name" CHARACTER VARYING(255) PRIMARY KEY, color CHARACTER VARYING(255), license INTEGER, weight FLOAT );
+    cur.execute("""
+    CREATE TABLE pets ( "name" CHARACTER VARYING(255) PRIMARY KEY, color CHARACTER VARYING(255), license INTEGER, weight FLOAT );
 """)
-#        self.connection.commit()
 
-    def tearDown(self):
-        self.connection.close()
-
+class Single:
     def test_single(self):
         upsert = Upsert(self.connection, 'pets')
         upsert.row({'name': 'Jerry'})
@@ -25,3 +22,74 @@ class TestUpsertPostgresql(unittest.TestCase):
         cur = self.connection.cursor()
         cur.execute("select name from pets;")
         self.assertEqual('Jerry', cur.fetchone()[0])
+
+
+class TestUpsertPostgresql(unittest.TestCase, Single):
+    def setUp(self):
+        self.connection = psycopg2.connect('dbname=test_py_upsert')
+        create_table(self.connection)
+
+    def tearDown(self):
+        self.connection.close()
+
+class TestUpsertMysql(unittest.TestCase, Single):
+    def setUp(self):
+        self.connection = MySQLdb.connect(user="root",passwd="password",db="test_py_upsert")
+        create_table(self.connection)
+
+    def tearDown(self):
+        self.connection.close()
+
+class TestUpsertSqlite(unittest.TestCase, Single):
+    def setUp(self):
+        self.connection = sqlite3.connect(':memory:')
+        create_table(self.connection)
+
+    def tearDown(self):
+        self.connection.close()
+
+# class TestUpsertMysql()
+
+# class TestUpsertMysql(unittest.TestCase):
+#     def setUp(self):
+#         self.connection = MySQLdb.connect(user="root",passwd="password",db="test_py_upsert")
+#         cur = self.connection.cursor()
+#         cur.execute("""
+#         DROP TABLE IF EXISTS pets;
+# """)
+#         cur.execute("""
+#         CREATE TABLE pets ( "name" CHARACTER VARYING(255) PRIMARY KEY, color CHARACTER VARYING(255), license INTEGER, weight FLOAT );
+# """)
+
+#     def tearDown(self):
+#         self.connection.close()
+
+#     def test_single(self):
+#         upsert = Upsert(self.connection, 'pets')
+#         upsert.row({'name': 'Pierre'})
+
+#         cur = self.connection.cursor()
+#         cur.execute("select name from pets;")
+#         self.assertEqual('Pierre', cur.fetchone()[0])
+
+# class TestUpsertMysql(unittest.TestCase):
+#     def setUp(self):
+#         self.connection = MySQLdb.connect(user="root",passwd="password",db="test_py_upsert")
+#         cur = self.connection.cursor()
+#         cur.execute("""
+#         DROP TABLE IF EXISTS pets;
+# """)
+#         cur.execute("""
+#         CREATE TABLE pets ( "name" CHARACTER VARYING(255) PRIMARY KEY, color CHARACTER VARYING(255), license INTEGER, weight FLOAT );
+# """)
+
+#     def tearDown(self):
+#         self.connection.close()
+
+#     def test_single(self):
+#         upsert = Upsert(self.connection, 'pets')
+#         upsert.row({'name': 'Pierre'})
+
+#         cur = self.connection.cursor()
+#         cur.execute("select name from pets;")
+#         self.assertEqual('Pierre', cur.fetchone()[0])
--- upsert.py
+++ upsert.py
@@ -1,9 +1,61 @@
-class Upsert:
-  def __init__(self, connection, table_name):
-    self.connection = connection
-    self.table_name = table_name
+import codecs
 
-  def row(self, selector):
-    template = 'INSERT INTO "%s" ("%s")' % (self.table_name, selector.keys()[0])
-    cur = self.connection.cursor()
-    cur.execute((template + " VALUES (%s)"), (selector.values()[0],))
+class Upsert:
+
+    def __init__(self, connection, table_name):
+        self.connection = connection
+        self.table_name = table_name
+
+    def row(self, selector):
+        values = (selector.values()[0],)
+        template = self.fill_ident_placeholders('INSERT INTO %s (%s)', (self.table_name, selector.keys()[0]))
+        self.execute((template + ' VALUES (%s) '), values)
+
+    def execute(self, template, values = ()):
+        return Upsert.execute_functions[self.type_key()](self, template, values)
+
+    # http://stackoverflow.com/questions/6514274/how-do-you-escape-strings-for-sqlite-table-column-names-in-python
+    def quote_ident(self, str, errors="strict"):
+        if self.type_key() == Upsert.mysql_key:
+            # unless it's in ANSI mode
+            return '`' + str + '`'
+        else:
+            encodable = str.encode("utf-8", errors).decode("utf-8")
+            nul_index = encodable.find("\x00")
+            if nul_index >= 0:
+                error = UnicodeEncodeError("NUL-terminated utf-8", encodable, nul_index, nul_index + 1, "NUL not allowed")
+                error_handler = codecs.lookup_error(errors)
+                replacement, _ = error_handler(error)
+                encodable = encodable.replace("\x00", replacement)
+            return '"' + encodable.replace('"', '""') + '"'
+    
+    def fill_ident_placeholders(self, template, idents):
+        quoted = tuple(self.quote_ident(str) for str in idents)
+        return template % quoted
+
+    def type_key(self):
+        return str(type(self.connection))
+
+    # so is this following DB-API and the others are breaking it?
+    def execute_sqlite3(self, template, values):
+        template = template.replace('%s', '?')
+        cur = self.connection.cursor()
+        cur.execute(template, values)
+
+    def execute_mysql(self, template, values):
+        cur = self.connection.cursor()
+        cur.execute(template, values)
+
+    def execute_postgresql(self, template, values):
+        cur = self.connection.cursor()
+        cur.execute(template, values)
+
+    sqlite3_key =     "<type 'sqlite3.Connection'>"
+    mysql_key =       "<class 'MySQLdb.connections.Connection'>"
+    postgresql_key =  "<type 'psycopg2._psycopg.connection'>"
+
+    execute_functions = {
+        sqlite3_key:    execute_sqlite3,
+        mysql_key:      execute_mysql,
+        postgresql_key: execute_postgresql
+    }
