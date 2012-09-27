03ce5e0b1938ae76274cb3610e39d40ba1df2dde
Seamus Abshere <seamus@abshere.net>
2012-09-26 20:00:19 -0500

    fix up

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ sys.path.append('upsert')
 import upsert
 
 setup(name='upsert',
+      packages=['upsert'],
       author='Seamus Abshere',
       author_email='seamus@abshere.net',
       url='https://github.com/seamusabshere/py-upsert',
@@ -12,10 +13,7 @@ setup(name='upsert',
       download_url='https://github.com/seamusabshere/py-upsert/zipball/v0.0.1',
       description='Upsert for MySQL, PostgreSQL, SQLite3.',
       long_description=upsert.Upsert.__doc__,
-      package_dir={'': 'upsert'},
-      py_modules=['upsert'],
-      provides=['upsert'],
-      keywords='upsert sql merge',
+      keywords=['upsert', 'sql', 'merge'],
       license='MIT',
       classifiers=[
           'Development Status :: 4 - Beta',
