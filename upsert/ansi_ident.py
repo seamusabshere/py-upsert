a4d2e5d3af5056759f984d60583c97fd9f6a88de
Seamus Abshere <seamus@abshere.net>
2012-09-26 19:45:10 -0500

    memoize

--- upsert/ansi_ident.py
+++ upsert/ansi_ident.py
@@ -1,9 +1,11 @@
 import codecs
+import upsert
 
 class AnsiIdent:
     # http://stackoverflow.com/questions/6514274/how-do-you-escape-strings-for-sqlite-table-column-names-in-python
-    def quote_ident(self, str, errors="strict"):
-        encodable = str.encode("utf-8", errors).decode("utf-8")
+    @upsert.memoize
+    def quote_ident(self, str):
+        encodable = str.encode("utf-8", "strict").decode("utf-8")
         nul_index = encodable.find("\x00")
         if nul_index >= 0:
             error = UnicodeEncodeError("NUL-terminated utf-8", encodable, nul_index, nul_index + 1, "NUL not allowed")
